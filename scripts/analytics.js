dd8558ee8acb04b5dd10a786541155a13ff543ee
milafrerichs <mila.frerichs@gmail.com>
2016-08-31 12:59:33 +0200

    Add drip analytics to page

---
+++ scripts/analytics.js
@@ -0,0 +1,11 @@
+var _dcq = _dcq || [];
+var _dcs = _dcs || {}; 
+_dcs.account = '8052806';
+
+(function() {
+  var dc = document.createElement('script');
+  dc.type = 'text/javascript'; dc.async = true; 
+  dc.src = '//tag.getdrip.com/8052806.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(dc, s);
+})();
